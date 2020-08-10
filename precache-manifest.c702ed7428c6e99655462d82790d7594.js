388f92faed24a889e9cfc5ef0358183dbca3ce70
Pramod N <npramod95@gmail.com>
2020-08-10 15:31:23 +0530

    Updates

---
+++ precache-manifest.c702ed7428c6e99655462d82790d7594.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a433eed29e571fb6e7c1f42c285019e",
+    "url": "/quora-clone/index.html"
+  },
+  {
+    "revision": "66c167800757bb28be25",
+    "url": "/quora-clone/static/css/2.f84572e1.chunk.css"
+  },
+  {
+    "revision": "c50ed999984cc503f760",
+    "url": "/quora-clone/static/css/main.e39eeca5.chunk.css"
+  },
+  {
+    "revision": "66c167800757bb28be25",
+    "url": "/quora-clone/static/js/2.a2c7182e.chunk.js"
+  },
+  {
+    "revision": "c50ed999984cc503f760",
+    "url": "/quora-clone/static/js/main.be3031af.chunk.js"
+  },
+  {
+    "revision": "75b55764d35f466aa79a",
+    "url": "/quora-clone/static/js/runtime-main.655bc822.js"
+  },
+  {
+    "revision": "60d230397edf78fe70dea244de1f0d1e",
+    "url": "/quora-clone/static/media/user_profile.60d23039.png"
+  }
+]);
--- src/reducers/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import FetchApiDataReducer from "./FetchApiDataReducer.js";
-
-const rootReducer = combineReducers({
-  apiData: FetchApiDataReducer
-});
-
-export default rootReducer;
